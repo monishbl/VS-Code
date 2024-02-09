eeb27fdef30d407d0c762ea4fa833bd851573a5e
Monish <monishbl159@gmail.com>
2024-02-09 12:34:57 +0530

    Read-Write

---
+++ NodeJS/read-write-file.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+fs.writeFile('message.txt', 'Hello World! 👋', (err)=> {
+    if (err) {
+        throw err;
+    }
+    console.log('File created');
+    
+});
+fs.readFile('message.txt', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    console.log(data.toString());
+});
